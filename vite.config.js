80688c968dfd604ce0f1f76b1430402acbb01057
Sassypsilon <89396210+Sassypsilon@users.noreply.github.com>
2025-07-20 18:17:51 +0200

    added tailwind and vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   root: 'frontend/public',
   build: {
     outDir: '../../dist',
     emptyOutDir: true
-  }
+  },
+  plugins: [
+    tailwindcss(),
+  ]
 });
